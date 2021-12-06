904456754703678aeef040ba7c65a260882b5690
Cafe-Worker7 <yoshimura.naoya@ist.osaka-u.ac.jp>
2021-12-06 03:52:09 +0000

    feat: remove capture #1

--- main.cpp
+++ main.cpp
@@ -116,174 +116,6 @@ static bool point_cloud_depth_to_color(k4a_transformation_t transformation_handl
     return true;
 }
 
-static int capture(std::string output_dir, uint8_t deviceId = K4A_DEVICE_DEFAULT)
-{
-    int returnCode = 1;
-    k4a_device_t device = NULL;
-    const int32_t TIMEOUT_IN_MS = 10000;
-    k4a_transformation_t transformation = NULL;
-    k4a_transformation_t transformation_color_downscaled = NULL;
-    k4a_capture_t capture = NULL;
-    std::string file_name = "";
-    uint32_t device_count = 0;
-    k4a_device_configuration_t config = K4A_DEVICE_CONFIG_INIT_DISABLE_ALL;
-    k4a_image_t depth_image = NULL;
-    k4a_image_t color_image = NULL;
-    k4a_image_t color_image_downscaled = NULL;
-
-    device_count = k4a_device_get_installed_count();
-
-    if (device_count == 0)
-    {
-        printf("No K4A devices found\n");
-        return 0;
-    }
-
-    if (K4A_RESULT_SUCCEEDED != k4a_device_open(deviceId, &device))
-    {
-        printf("Failed to open device\n");
-        goto Exit;
-    }
-
-    config.color_format = K4A_IMAGE_FORMAT_COLOR_BGRA32;
-    config.color_resolution = K4A_COLOR_RESOLUTION_720P;
-    config.depth_mode = K4A_DEPTH_MODE_NFOV_UNBINNED;
-    config.camera_fps = K4A_FRAMES_PER_SECOND_30;
-    config.synchronized_images_only = true; // ensures that depth and color images are both available in the capture
-
-    k4a_calibration_t calibration;
-    if (K4A_RESULT_SUCCEEDED !=
-        k4a_device_get_calibration(device, config.depth_mode, config.color_resolution, &calibration))
-    {
-        printf("Failed to get calibration\n");
-        goto Exit;
-    }
-
-    transformation = k4a_transformation_create(&calibration);
-
-    if (K4A_RESULT_SUCCEEDED != k4a_device_start_cameras(device, &config))
-    {
-        printf("Failed to start cameras\n");
-        goto Exit;
-    }
-
-    // Get a capture
-    switch (k4a_device_get_capture(device, &capture, TIMEOUT_IN_MS))
-    {
-    case K4A_WAIT_RESULT_SUCCEEDED:
-        break;
-    case K4A_WAIT_RESULT_TIMEOUT:
-        printf("Timed out waiting for a capture\n");
-        goto Exit;
-    case K4A_WAIT_RESULT_FAILED:
-        printf("Failed to read a capture\n");
-        goto Exit;
-    }
-
-    // Get a depth image
-    depth_image = k4a_capture_get_depth_image(capture);
-    if (depth_image == 0)
-    {
-        printf("Failed to get depth image from capture\n");
-        goto Exit;
-    }
-
-    // Get a color image
-    color_image = k4a_capture_get_color_image(capture);
-    if (color_image == 0)
-    {
-        printf("Failed to get color image from capture\n");
-        goto Exit;
-    }
-
-    // Compute color point cloud by warping color image into depth camera geometry
-#ifdef _WIN32
-    file_name = output_dir + "\\color_to_depth.ply";
-#else
-    file_name = output_dir + "/color_to_depth.ply";
-#endif
-    if (point_cloud_color_to_depth(transformation, depth_image, color_image, file_name.c_str()) == false)
-    {
-        goto Exit;
-    }
-
-    // Compute color point cloud by warping depth image into color camera geometry
-#ifdef _WIN32
-    file_name = output_dir + "\\depth_to_color.ply";
-#else
-    file_name = output_dir + "/depth_to_color.ply";
-#endif
-    if (point_cloud_depth_to_color(transformation, depth_image, color_image, file_name.c_str()) == false)
-    {
-        goto Exit;
-    }
-
-    // Compute color point cloud by warping depth image into color camera geometry with downscaled color image and
-    // downscaled calibration. This example's goal is to show how to configure the calibration and use the
-    // transformation API as it is when the user does not need a point cloud from high resolution transformed depth
-    // image. The downscaling method here is naively to average binning 2x2 pixels, user should choose their own
-    // appropriate downscale method on the color image, this example is only demonstrating the idea. However, no matter
-    // what scale you choose to downscale the color image, please keep the aspect ratio unchanged (to ensure the
-    // distortion parameters from original calibration can still be used for the downscaled image).
-    k4a_calibration_t calibration_color_downscaled;
-    memcpy(&calibration_color_downscaled, &calibration, sizeof(k4a_calibration_t));
-    calibration_color_downscaled.color_camera_calibration.resolution_width /= 2;
-    calibration_color_downscaled.color_camera_calibration.resolution_height /= 2;
-    calibration_color_downscaled.color_camera_calibration.intrinsics.parameters.param.cx /= 2;
-    calibration_color_downscaled.color_camera_calibration.intrinsics.parameters.param.cy /= 2;
-    calibration_color_downscaled.color_camera_calibration.intrinsics.parameters.param.fx /= 2;
-    calibration_color_downscaled.color_camera_calibration.intrinsics.parameters.param.fy /= 2;
-    transformation_color_downscaled = k4a_transformation_create(&calibration_color_downscaled);
-    color_image_downscaled = downscale_image_2x2_binning(color_image);
-    if (color_image_downscaled == 0)
-    {
-        printf("Failed to downscaled color image\n");
-        goto Exit;
-    }
-
-#ifdef _WIN32
-    file_name = output_dir + "\\depth_to_color_downscaled.ply";
-#else
-    file_name = output_dir + "/depth_to_color_downscaled.ply";
-#endif
-    if (point_cloud_depth_to_color(transformation_color_downscaled,
-                                   depth_image,
-                                   color_image_downscaled,
-                                   file_name.c_str()) == false)
-    {
-        goto Exit;
-    }
-
-    returnCode = 0;
-
-Exit:
-    if (depth_image != NULL)
-    {
-        k4a_image_release(depth_image);
-    }
-    if (color_image != NULL)
-    {
-        k4a_image_release(color_image);
-    }
-    if (capture != NULL)
-    {
-        k4a_capture_release(capture);
-    }
-    if (transformation != NULL)
-    {
-        k4a_transformation_destroy(transformation);
-    }
-    if (transformation_color_downscaled != NULL)
-    {
-        k4a_transformation_destroy(transformation_color_downscaled);
-    }
-    if (device != NULL)
-    {
-        k4a_device_close(device);
-    }
-    return returnCode;
-}
-
 // Timestamp in milliseconds. Defaults to 1 sec as the first couple frames don't contain color
 static int playback(char *input_path, int timestamp = 1000, std::string output_filename = "output.ply")
 {
@@ -435,7 +267,6 @@ Exit:
 
 static void print_usage()
 {
-    printf("Usage: transformation_example capture <output_directory> [device_id]\n");
     printf("Usage: transformation_example playback <filename.mkv> [timestamp (ms)] [output_file]\n");
 }
 
@@ -450,22 +281,7 @@ int main(int argc, char **argv)
     else
     {
         std::string mode = std::string(argv[1]);
-        if (mode == "capture")
-        {
-            if (argc == 3)
-            {
-                returnCode = capture(argv[2]);
-            }
-            else if (argc == 4)
-            {
-                returnCode = capture(argv[2], (uint8_t)atoi(argv[3]));
-            }
-            else
-            {
-                print_usage();
-            }
-        }
-        else if (mode == "playback")
+        if (mode == "playback")
         {
             if (argc == 3)
             {
